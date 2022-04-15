599b2cfc88cc5ef0839fa62b6dce0e8b94c583a4
keivanh00 <61167315+keivanh00@users.noreply.github.com>
2022-04-14 19:42:16 -0700

    Added icons and updated home page

--- ekea/lib/main.dart
+++ ekea/lib/main.dart
@@ -1,30 +1,53 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: Center(
-      child: ListView(
-        children: <Widget>[
-          Text('one'),
-          Text('one'),
-          Text('one'),
-          Text('one'),
-          // Text('one'),
-          // Text('one'),
-          // Text('one'),
-          // Text('one'),
-          // Text('one'),
-          // Text('one'),
-          // Text('one'),
-          // Text('one'),
-          // Text('one'),
-          // Text('one'),
-          // Text('one'),
-          // Text('one'),
-          // Text('one'),
-          // Text('one'),
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    ),
+  );
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: new AppBar(
+        backgroundColor: Colors.red,
+        title: Text('EKEA'),
+        actions: [
+          new IconButton(
+              icon: Icon(
+                Icons.search,
+                color: Colors.white,
+              ),
+              onPressed: () {}),
+          new IconButton(
+              icon: Icon(
+                Icons.shopping_cart,
+                color: Colors.white,
+              ),
+              onPressed: () {})
         ],
       ),
-    ),
-  ));
+      drawer: new Drawer(
+        child: new ListView(
+          children: [
+            //header of drawer
+            new UserAccountsDrawerHeader(
+                accountName: Text('Cmpe 137'),
+                accountEmail: Text('Cmpe137@sjsu.edu'))
+          ],
+        ),
+      ),
+    );
+  }
 }
