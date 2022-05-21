029b12d574239af88170f21163414aea46420e43
Anvay Bhanap <anvay.bhanap@gmail.com>
2022-05-21 15:27:20 -0700

    finished confirmed page

--- ekea/lib/pages/account.dart
+++ ekea/lib/pages/account.dart
@@ -21,9 +21,9 @@ class _AccountState extends State<Account> {
       child: Container(
           // color: Colors.white,
 
-          child: Row(
+          child: Column(
             children:  <Widget>[
-              Expanded(
+              Container(
                   child: Padding (
                     padding: EdgeInsets.fromLTRB(20, 0, 0, 30),
 
@@ -34,13 +34,13 @@ class _AccountState extends State<Account> {
 
               ),
 
-              const Expanded(
+              const Center(
                 child: Padding(
                     padding: EdgeInsets.fromLTRB(0, 0, 0, 40),
                     child: ListTile(
                       title: Text("Name: \nSammy Spartan \n"),
                       subtitle: Text(
-                          "Email: \nsammy.spartan@sjsu.edu \n\nCredit Card: \nXXX-XXX-XXX-XX-0126",
+                          "Email: \nsammy.spartan@sjsu.edu \n\nCredit Card: \nXXX-XXX-XXX-XX-0126 \n\nAddress: \n1 Washington Sq, San Jose, CA 95192",
                         style: TextStyle(fontStyle: FontStyle.italic, fontSize: 14),
                       ),
                       isThreeLine: true,
