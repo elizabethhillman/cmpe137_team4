a3bd77ef1c0790b2e3fffec2e19be77f7631f747
Eric Dou <ericdou@gmail.com>
2022-05-21 18:43:56 -0700

    added cart navigation back

--- ekea/lib/pages/product_details.dart
+++ ekea/lib/pages/product_details.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:ekea/main.dart';
+import 'package:ekea/pages/cart.dart';
 
 class ProductDetails extends StatefulWidget {
   final product_detail_name;
